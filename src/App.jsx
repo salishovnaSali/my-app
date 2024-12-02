6a5889ad6834ce12e01c274ec34a4c7a9b488841
Saltanat <saltanatabdylasova450@gmail.com>
2024-12-02 14:55:45 +0600

    first name

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import ff from "./assets/ff.jpg"
+import nn from "./assets/nn.jpg"
+import oo from "./assets/oo.jpg"
+import ooo from "./assets/ooo.jpg"
+import ww from "./assets/ww.jpeg"
+import images from "./assets/images.jpeg"
+import mainn from "./assets/mainn.webp"
+import main from "./assets/main.jpg"
+
+
+function App() {
+  return (
+    <div className='app'>
+      <img src={ff} alt="" />
+      <img src={nn} alt="" />
+      <img src={oo} alt="" />
+      <img src={ooo} alt="" />
+      <img src={ww} alt="" />
+      <img src={images} alt="" />
+      <img src={mainn} alt="" />
+      <img src={main} alt="" />
+    </div>
+  )
+}
+
+export default App
